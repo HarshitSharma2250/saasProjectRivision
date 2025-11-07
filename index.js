1eed8b5ce63b483779fbcef8f2f9c301495e9bc2
Harshit Sharma <121739676+HarshitSharma2250@users.noreply.github.com>
2025-11-07 18:58:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 // ✅ Setup Socket.IO on top of HTTP server
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000" || "https://saasadmindashboard.netlify.app/login",
     credentials: true,
   },
 });
